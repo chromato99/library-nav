951898d6e4a336089e3b2689b58086f764cc6d57
chromato99 <chromato99@gmail.com>
2021-10-29 21:04:05 +0900

    remove wrong modules

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const compression = require('compression');
 const front = require('./lib/front.js');
-var helmet = require('helmet');
 var parseurl = require('parseurl');
-var flash = require('connect-flash');
 
 const app = express();
 const port = 80
 
-app.use(helmet());
 app.use(compression());
 app.use(express.static('public'));
-
-app.use(flash());
 
 app.use(express.urlencoded({ extended: false}));
 
