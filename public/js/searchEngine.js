3dead92038eb3337ef304ceece97c4444038c32f
chromato99 <chromato99@gmail.com>
2021-11-04 19:29:33 +0900

    now using database

--- public/js/searchEngine.js
+++ public/js/searchEngine.js
@@ -2,14 +2,3 @@ var remove = document.querySelectorAll(".close");
 for(var i = 0; i < remove.length; i++) {  
     remove[i].addEventListener("click", removeList);  
 }
-
-var addBtn = document.querySelector("#add");
-addBtn.addEventListener("click", addList);
-
-
-function removeList() {
-    var id = this.getAttribute("id"); 
-    itemList.splice(id, 1);  
-    showList(); 
-}
-
--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/search', (req, res, next) => {
     search_page.search(req, res, next);
 });
 
+app.get('/info/:name', (req, res, next) => {
+    search_page.info(req, res, next);
+});
+
 
 app.use((req, res, next) => {
     res.status(404).send('404 error');
